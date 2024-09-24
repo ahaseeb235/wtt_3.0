8531329e7dda01a52bf0ead31bea29435221100d
ahaseeb235 <ahaseeb235@gmail.com>
2024-09-25 00:18:36 +0100

    another app.py fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-# from flask import Flask
-from application.routes import app
-# from application import app
+from flask import Flask
+# from app.routes import app, db
+from application import app
 
 if __name__ == "__main__":
     app.run(debug = True)
