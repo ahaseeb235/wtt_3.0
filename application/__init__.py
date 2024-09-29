b98cfbbcb20c778657512393a536743d0ac13194
ahaseeb235 <ahaseeb235@gmail.com>
2024-09-29 16:13:22 +0100

    fix init.y

---
+++ application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///worktimeDB.db'
+app.config['SECRET_KEY'] = 'pa55word!'
+
+db = SQLAlchemy(app)
+
+
+from application import routes
